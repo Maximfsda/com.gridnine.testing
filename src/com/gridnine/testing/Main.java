98ce28a626a8a61590b2e9d81cf62f17f46f0469
Maximfsda <lisitsin.maxi@yandex.ru>
2023-06-13 00:52:54 +0700

    eqwedw

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Flight> flights = FlightBuilder.createFlights();
 
         FlightFilter departureBeforeNow = new DepartureBeforeNow();
